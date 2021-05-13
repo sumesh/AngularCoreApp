c9fb7e7cc720bc7b3bcc7119ec42b4171277fe6b
sumesh <sparakkat@gmail.com>
2021-05-13 23:50:44 +0530

    MVC default Views added for Redirection

--- Startup.cs
+++ Startup.cs
@@ -44,7 +44,7 @@ namespace AngularCoreApp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -62,7 +62,7 @@ namespace AngularCoreApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
 
             //app.UseSpa(spa =>
